be21ab9ff5a4026a272b7aafc73b9ce8bce00eb7
CodeItQuick <evanontario@hotmail.com>
2023-11-04 22:22:48 -0300

    split out domain tests

---
+++ Tests/DomainTests.cs
@@ -0,0 +1,33 @@
+ using Application.Services;
+ using Domain;
+ using ExpenseReportCSharp.Adapter;
+ using Expenses = Application.Services.Expenses;
+
+ namespace Tests;
+
+public class DomainTests
+{
+    // Unit Tests on Domain
+    [Fact]
+    public void CanAddAMealExpense()
+    {
+        var expenseReport = new ExpenseReport(new List<Expense>());
+
+        Expense firstExpense = new Expense(ExpenseType.BREAKFAST, 500);
+        expenseReport.AddExpense(firstExpense);
+
+        Assert.Equal(500, expenseReport.CalculateMealExpenses());
+    }
+    [Fact]
+    public void CanAddASecondExpense()
+    {
+        var expenseReport = new ExpenseReport(new List<Expense>());
+        Expense firstExpense = new Expense(ExpenseType.BREAKFAST, 500);
+        expenseReport.AddExpense(firstExpense);
+
+        Expense secondExpense = new Expense(ExpenseType.DINNER, 5010);
+        expenseReport.AddExpense(secondExpense);
+
+        Assert.Equal(5510, expenseReport.CalculateMealExpenses());
+    }
+}
