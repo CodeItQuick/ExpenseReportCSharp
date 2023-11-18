f023f3daeab0895305ac8e53432f14e7f3e89d57
CodeItQuick <evanontario@hotmail.com>
2023-11-18 16:41:04 -0400

    improve domain tests

--- Tests/Domain/ExpenseListTests.cs
+++ Tests/Domain/ExpenseListTests.cs
@@ -15,6 +15,16 @@ public class ExpenseListTests
         Assert.Equal(0, calculateMealExpenses);
     }
     [Fact]
+    public void CanCalculateOneMealExpense()
+    {
+        var expenseList = new ExpenseList(
+            new List<Expense>() { new Expense(ExpenseType.DINNER, 1000) }, 1);
+
+        var calculateMealExpenses = expenseList.CalculateMealExpenses();
+        
+        Assert.Equal(1000, calculateMealExpenses);
+    }
+    [Fact]
     public void CanRetrieveIndividualExpenses()
     {
         var expenseList = new ExpenseList(
@@ -23,6 +33,16 @@ public class ExpenseListTests
         var calculateIndividualExpenses = expenseList.RetrieveIndividualExpenses();
         
         Assert.Equal(0, calculateIndividualExpenses.Count);
+    }
+    [Fact]
+    public void CanRetrieveSingleIndividualExpenses()
+    {
+        var expenseList = new ExpenseList(
+            new List<Expense>() { new Expense(ExpenseType.CAR_RENTAL, 1111) }, 1);
+
+        var calculateIndividualExpenses = expenseList.RetrieveIndividualExpenses();
+        
+        Assert.Equal(1, calculateIndividualExpenses.Count);
     }
     [Fact]
     public void CanCalculateTotalExpenses()
@@ -35,13 +55,13 @@ public class ExpenseListTests
         Assert.Equal(0, calculateIndividualExpenses);
     }
     [Fact]
-    public void CanRetrieveExpenseListDate()
+    public void CanCalculateATotalExpense()
     {
         var expenseList = new ExpenseList(
-            new List<Expense>(), 1);
+            new List<Expense>() { new Expense(ExpenseType.DINNER, 1234) }, 1);
 
         var calculateIndividualExpenses = expenseList.CalculateTotalExpenses();
         
-        Assert.Equal(0, calculateIndividualExpenses);
+        Assert.Equal(1234, calculateIndividualExpenses);
     }
 }
