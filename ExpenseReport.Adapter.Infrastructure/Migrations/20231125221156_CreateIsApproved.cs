2f6b43e86bb0a663203a45dfb9e09f59e059e526
CodeItQuick <evanontario@hotmail.com>
2023-11-25 18:39:42 -0400

    add migrations

---
+++ ExpenseReport.Adapter.Infrastructure/Migrations/20231125221156_CreateIsApproved.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseReportCSharp.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateIsApproved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "ExpenseReport",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "ExpenseReport");
+        }
+    }
+}
