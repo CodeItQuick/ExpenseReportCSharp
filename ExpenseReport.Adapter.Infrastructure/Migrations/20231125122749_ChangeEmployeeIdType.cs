a30fe5d2f664a37706b9895f7c4f48d33367d077
CodeItQuick <evanontario@hotmail.com>
2023-11-25 09:58:52 -0400

    db migration

---
+++ ExpenseReport.Adapter.Infrastructure/Migrations/20231125122749_ChangeEmployeeIdType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseReportCSharp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeEmployeeIdType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmployeeId",
+                table: "ExpenseReport",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "EmployeeId",
+                table: "ExpenseReport",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
